e4d60027f2fa96aca00f99741062694462a768e9
Hoan Truong <hoantruong6814@gmail.com>
2020-11-06 20:17:52 +0700

    done layout page

--- home/views.py
+++ home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, 'pages/homePage.html')
+
+def dataAnalysis(request):
+  return render(request, 'pages/dataAnalysis.html')
+
+def simpleChart(request):
+  return render(request, 'components/scatter.html')
